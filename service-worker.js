6c5b62ce8a266f6764f76da01292e0843637dd6c
Bigitius <bigitius@users.noreply.github.com>
2026-07-18 20:13:11 +0200

    Service worker cache verzio bumpolasa v4-re (iPad meg regi verziot cachelt)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'fodrasz-foglalo-v3';
+﻿const CACHE_NAME = 'fodrasz-foglalo-v4';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './service-worker.js', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
